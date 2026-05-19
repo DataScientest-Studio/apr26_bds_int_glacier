835289ea3e98f5a413a392f8b61e1757be62ec5b
Theresa Hayek <thyk@gmx.de>
2026-05-19 16:53:34 +0200

    Fixed typos

--- reports/create_datasheet.py
+++ reports/create_datasheet.py
@@ -44,90 +44,90 @@ df['percentage of missing values'] = df['name of column'].map(lambda x: df_resul
 df['categorical/quantitative'] = 'quantitative'
 
 # variable for current measurement station
-measurement_station = 'Pegelstation Meteorologie'
+measurement_station = 'Pegelstation Meteorologie, height 2640 m'
 
 # Add information to df about column 'PM_temperature'
 column_name = 'PM_temperature'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Air temperature at 2 m height'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = '°C'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Pt-100 temperature sensor with ventilation fan'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Pt-100 temperature sensor with ventilation fan'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = '°C'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # Add information to df about column 'PM_atmospheric_pressure'
 column_name = 'PM_atmospheric_pressure'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Pressure, atmospheric'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'hPa'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Barometric pressure sensor, Druck, RPT 410h'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Barometric pressure sensor, Druck, RPT 410h'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'hPa'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # Add information to df about column 'PM_relative_humidity'
 column_name = 'PM_relative_humidity'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Humidity, relative'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = '%'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Hair hygrometer, Thies Clima'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Hair hygrometer, Thies Clima'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = '%'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # Add information to df about column 'PM_precipitation'
 column_name = 'PM_precipitation'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Precipitation'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'mm/5 min'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Tipping bucket, Gertsch, unheated'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Tipping bucket, Gertsch, unheated'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'mm/5 min'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # Add information to df about column 'PM_precipitation_cum'
 column_name = 'PM_precipitation_cum'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Precipitation, sum'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'mm'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Weighing rain gauge, Belfort'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Weighing rain gauge, Belfort'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'mm'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # Add information to df about column 'PM_snow_height'
 column_name = 'PM_snow_height'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Snow height'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'm'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Sonic Ranging Sensor, Campbell Scientific, SR50'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Sonic Ranging Sensor, Campbell Scientific, SR50'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'm'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # Add information to df about column 'PM_wind_speed'
 column_name = 'PM_wind_speed'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Wind speed at 2 m height'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'm/s'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Anemometer, Thies Clima'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Anemometer, Thies Clima'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'm/s'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # Add information to df about column 'PM_wind_direction'
 column_name = 'PM_wind_direction'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Wind direction at 2 m height'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'deg'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Wind vane, Thies Clima'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Wind vane, Thies Clima'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'deg'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # Add information to df about column 'PM_SWD'
 column_name = 'PM_SWD'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Short-wave downward (GLOBAL) radiation'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'W/m**2'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Albedometer, Kipp & Zonen, CM7B, unventilated'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Albedometer, Kipp & Zonen, CM7B, unventilated'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'W/m**2'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # Add information to df about column 'PM_SWU'
 column_name = 'PM_SWU'
 df.loc[df['name of column'] == column_name, 'name of variable'] = 'Short-wave upward (REFLEX) radiation'
 df.loc[df['name of column'] == column_name, 'name of measurement station'] = measurement_station
-df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'W/m**2'
-df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'Albedometer, Kipp & Zonen, CM7B, unventilated'
+df.loc[df['name of column'] == column_name, 'measurement instrument'] = 'Albedometer, Kipp & Zonen, CM7B, unventilated'
+df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'W/m**2'
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # variable for current measurement station
-measurement_station = 'Schwarzkögele'
+measurement_station = 'Schwarzkögele, height 3075 m'
 
 # Add information to df about column 'SK_temperature'
 column_name = 'SK_temperature'
@@ -162,7 +162,7 @@ df.loc[df['name of column'] == column_name, 'measurement unit of variable'] = 'd
 df.loc[df['name of column'] == column_name, 'description'] = ''
 
 # variable for current measurement station
-measurement_station = 'Pegelstation Hydrologie'
+measurement_station = 'Pegelstation Hydrologie, height 2640 m'
 
 # Add information to df about column 'water_level'
 column_name = 'water_level'
